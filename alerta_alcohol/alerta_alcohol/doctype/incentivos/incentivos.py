aee64e9f1c3bc3e5597a1d9e7f6723490caf3646
Nirchains <nirchains@gmail.com>
2020-09-04 06:22:06 +0200

    primera prueba

--- alerta_alcohol/alerta_alcohol/doctype/incentivos/incentivos.py
+++ alerta_alcohol/alerta_alcohol/doctype/incentivos/incentivos.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe.utils import nowdate, cstr, flt, cint, now, getdate
 from frappe.model.document import Document
 
 class Incentivos(Document):
-	pass
+	def validate(self):
+		contador = cint(frappe.db.count("Incentivos", {"username": self.username, "project": self.project, "card": self.card}))
+		#frappe.log_error("contador: {0}".format(contador))
+		if contador > 0:
+			frappe.throw("Ya ha conseguido esta carta")
---
+++ alerta_alcohol/public/cors/puntuaciones.js
@@ -0,0 +1,52 @@
+//var host = "http://pre.alertaalcohol.com";
+//var apiKey = "215e8d8fd8ea1e8";
+//var apiSecret = "63b17f0ebe96d5d";
+
+const headers = new Headers();
+const token = "token " + apiKey + ":" + apiSecret
+headers.append('Authorization', token);
+headers.append('cache-control', 'no-cache');
+headers.append('Accept', 'application/json');
+headers.append('Content-Type', 'application/json');
+function getCard(username, project, card, host, apiKey, apiSecret) {
+	//var url0 = host + "/api/resource/Incentivos?fields=[%22username%22,%20%22project%22,%20%22card%22]";
+	var url = host + "/api/resource/Carta/" + card;
+			
+	fetch(url, {
+		headers: {
+			'Authorization': token
+		}
+	})
+	.then(r => r.json())
+	.then(r => {
+		console.log(r.data.image);
+		var card_element = document.getElementById('card');
+		card_element.outerHTML = "<div id='card'><img src='" + host + "" + r.data.image + "' alt='funciona' /></div>";
+		var card_container = document.getElementById('card-container');
+		//card_element.classList.remove("card-0");
+		card_container.classList.add("card-1");
+	})
+
+	const data = {
+			username: username,
+			project: project,
+			card: card
+		}
+	const init = {
+		method: 'POST',
+		headers,
+		body: JSON.stringify(data)
+	}
+
+	const urll = host + "/api/resource/Incentivos"
+	fetch(urll, init)
+	.then((response) => {
+		return response.text(); // or .json() or .blob() ...
+	})
+	.then((text) => {
+		console.log(text) // text is the response body
+	})
+	.catch((e) => {
+		console.log(e) // error in e.message
+	});
+}
